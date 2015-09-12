104f8e3c0d5ccfe2516b52d14a690a66233334a4
RoboticsAlmaliah2015 <robotics@spam4.me>
2015-09-12 14:23:29 +0300

    First commit

---
+++ WebFinalProject/WebFinalProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFinalProject.Startup))]
+namespace WebFinalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
