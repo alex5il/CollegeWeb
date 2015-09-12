9b56e42a03465081e19b16c4eba100cccc593d69
alex5il <alexkono91@gmail.com>
2015-09-12 20:58:11 +0300

    Pre-conflict

--- WebFinalProject/WebFinalProject/Controllers/AccountController.cs
+++ WebFinalProject/WebFinalProject/Controllers/AccountController.cs
@@ -155,6 +155,10 @@ namespace WebFinalProject.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //Assign Role to user Here 
+                    await this.UserManager.AddToRoleAsync(user.Id, "User");
+                    //Ends Here
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
