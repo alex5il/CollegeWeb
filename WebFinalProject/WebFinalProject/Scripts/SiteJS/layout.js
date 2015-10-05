e06a5bb28272496d31b34196cc4d73f7c11c3f5f
alex5il <alexkono91@gmail.com>
2015-10-05 14:55:42 +0300

    logo and some layout

--- WebFinalProject/WebFinalProject/Scripts/SiteJS/layout.js
+++ WebFinalProject/WebFinalProject/Scripts/SiteJS/layout.js
@@ -10,6 +10,29 @@
             ctx.fillStyle = "#B5D6C0";
             ctx.fillText("Giga games", 0, 20);
         }
+
+        // canvas event listeners
+
+        // Start animating when the mouse enters the canvas
+        canvas.addEventListener('mouseover', function (e) {
+            redraw("red", true);
+        });
+
+        // Stop animating when the mouse exits the canvas
+        canvas.addEventListener('mouseout', function (e) {
+            redraw("#B5D6C0");
+        });
+
+        function redraw(color, isOver) {
+            ctx.save();
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            ctx.font = "28px Comic Sans MS";
+            ctx.fillStyle = color;
+            ctx.fillText("Giga games", 0, 20);
+
+            ctx.restore();
+        }
     }
 
     draw();
